584be471149300e8635520ee72c7f69494a3617e
Luna Leguisamo <9356@holbertonstudents.com>
2024-12-04 19:15:58 -0300

    6

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-from typing import Union, List
 """
 Module that define a function that return a sum
 of integers and floats in a list
 """
+from typing import Union, List
 
 
 def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
