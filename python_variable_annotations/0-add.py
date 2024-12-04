7a1c80e072a8ba59e7911aee7c456b3cafb2d873
Luna Leguisamo <9356@holbertonstudents.com>
2024-12-04 18:07:45 -0300

    1

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+"""
+Module that define a function that return an add
+of two arguments
+"""
+
+def add(a: float, b: float) -> str:
+    """_summary_
+
+    Args:
+        a (float): _description_
+        b (float): _description_
+
+    Returns:
+        str: _description_
+    """
+    return a + b
