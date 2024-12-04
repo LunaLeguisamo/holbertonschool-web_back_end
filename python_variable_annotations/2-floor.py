a715b92d663e81ead09471760bdbaee689d4d849
Luna Leguisamo <9356@holbertonstudents.com>
2024-12-04 18:17:26 -0300

    floor

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+"""
+Module that define a function that return an
+concatenate string
+"""
+
+
+def floor(n: float) -> float:
+    """_summary_
+
+    Args:
+        n (float): _description_
+
+    Returns:
+        float: _description_
+    """
+    return floor(n)
