ba098e369263f99b49830c2c19124f58e0284830
Luna Leguisamo <9356@holbertonstudents.com>
2024-12-04 18:12:15 -0300

    concat

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+"""
+Module that define a function that return an
+concatenate string
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """_summary_
+
+    Args:
+        str1 (str): _description_
+        str2 (str): _description_
+
+    Returns:
+        str: _description_
+    """
+    return str1 + str2
