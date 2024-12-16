22d821049e8c31cd75aa2faf9b4b25bd25bba557
Luna Leguisamo <9356@holbertonstudents.com>
2024-12-15 22:18:35 -0300

    3.2 comments

--- pagination/3-hypermedia_del_pagination.py
+++ pagination/3-hypermedia_del_pagination.py
@@ -40,6 +40,26 @@ class Server:
         return self.__indexed_dataset
 
     def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
+        """
+        This method returns a paginated page of the dataset, resilient to
+        deletions.
+        It takes the starting index (index) and the page size (page_size),
+        and return a dictionary with:
+        - 'index': the starting index of the page.
+        - 'next_index': the index of the next item to query, or None if
+        the page is the last one.
+        - 'page_size': the number of items in the current page.
+        - 'data': the list of items on the current page.
+
+        Parameters:
+        - index (int): The index to start the pagination from. Default is 0.
+        - page_size (int): The number of items to show on each page.
+        Default is 10.
+
+        Returns:
+        - A dictionary containing pagination information.
+        """
+
         new_dataset = []
         data = self.indexed_dataset()
         current_index = index
